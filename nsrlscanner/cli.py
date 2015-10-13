d9bb3ce8de6799b0dc494ebb3ab87fb207cc7d4d
Brandon Kreisel <kreiselb@gmail.com>
2015-10-12 21:50:25 -0400

    Make Linter Happy (Docstring Party!)

--- nsrlscanner/cli.py
+++ nsrlscanner/cli.py
@@ -1,19 +1,26 @@
+""" NSRLScanner CLI"""
 import argparse
+import sys
 
 def create_parser():
+    """ Create Parser Object
+
+        :returns: parser object
+    """
     parser = argparse.ArgumentParser(
         description='Check a directory against NIST NSRL Database'
     )
 
     parser.add_argument(
-        'nsrl_db',help='TXT file containing NSRL Database'
+        'nsrl_db', help='TXT file containing NSRL Database'
     )
 
     return parser
 
 def main():
+    """CLI Main Function and Argument Handler"""
     parser = create_parser()
     args = parser.parse_args()
 
 if __name__ == '__main__':
-    main()
+    sys.exit(main())

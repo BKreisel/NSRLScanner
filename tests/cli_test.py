37ea53e3cdac531f9dc49d257d598380ce7b7245
Brandon Kreisel <kreiselb@gmail.com>
2015-12-09 20:13:36 -0500

    Add Tests for PATH argument

--- tests/cli_test.py
+++ tests/cli_test.py
@@ -16,8 +16,13 @@ class ArgTests(CLITestClass):
             self.parser.parse_args([])
 
     def test_one_arg(self):
-        args = self.parser.parse_args(['sample.txt'])
-        self.assertIsNotNone(args.nsrl_db)
+        with self.assertRaises(SystemExit):
+            self.parser.parse_args(['sample.txt'])
 
+    def test_all_args(self):
+        args = self.parser.parse_args(['sample.txt','path'])
+        self.assertIsNotNone(args.nsrl_db)
+        self.assertIsNotNone(args.path)
+        
 if __name__ == '__main__':
     unittest.main()

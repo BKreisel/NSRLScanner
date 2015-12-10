0de344da08a5822164d463104841b423425e849b
Brandon Kreisel <kreiselb@gmail.com>
2015-12-09 21:13:02 -0500

    Implement Path Validation

--- nsrlscanner/cli.py
+++ nsrlscanner/cli.py
@@ -1,5 +1,6 @@
 """ NSRLScanner CLI"""
 import argparse
+import os
 import sys
 
 import database as db
@@ -21,11 +22,30 @@ def create_parser():
     )
     return parser
 
+def validate_path(path):
+    """ Ensures Path is Real File or Directory
+
+        :arg path: String containing path
+
+        :returns: Boolean for file validity
+    """
+    print(path)
+    path = os.path.expandvars(path)
+    print(path)
+    if os.path.exists(path):
+        return True
+    return False
+
+
 def main():
     """CLI Main Function and Argument Handler"""
     parser = create_parser()
     args = parser.parse_args()
 
+    if not validate_path(args.path):
+        print("[-] Error: Path \"" + args.path + "\" does not exist")
+        sys.exit(1)
+        
     db_file = db.load_database(args.nsrl_db)
 
 if __name__ == '__main__':

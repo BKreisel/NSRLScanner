6c911763b3ae7636d4ff380bb141e75b87e9c643
Brandon Kreisel <kreiselb@gmail.com>
2015-12-09 20:44:45 -0500

    Add Path Tests

--- tests/cli_test.py
+++ tests/cli_test.py
@@ -2,6 +2,7 @@ import os
 import unittest
 
 from nsrlscanner import cli
+from tests.database_test import write_tmp_file
 
 class CLITestClass(unittest.TestCase):
     @classmethod
@@ -23,6 +24,24 @@ class ArgTests(CLITestClass):
         args = self.parser.parse_args(['sample.txt','path'])
         self.assertIsNotNone(args.nsrl_db)
         self.assertIsNotNone(args.path)
-        
+
+
+class PathTests(unittest.TestCase):
+
+    def test_bad_file(self):
+        self.assertFalse(cli.validate_path('/filethatdoesntexist'))
+    def test_good_file(self):
+        write_tmp_file("test_file","")
+        self.assertTrue(cli.validate_path('test_file'))
+        os.remove("test_file")
+    def test_bad_dir(self):
+        self.assertFalse(cli.validate_path('/dirthatdoesntexist/'))
+    def test_good_dir(self):
+        os.makedirs('test_dir')
+        self.assertTrue(cli.validate_path('test_dir'))
+        os.rmdir('test_dir')
+    def test_expand_dir(self):
+        self.assertTrue(cli.validate_path('~'))
+
 if __name__ == '__main__':
     unittest.main()
